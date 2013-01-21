1af0529ada65b6bd4aba907376c560fc4c05b3ac
Stefan Wienert <stefan.wienert@pludoni.de>
2013-01-21 22:20:00 +0100

    Fonts; Configs; Stylings

--- config/application.rb
+++ config/application.rb
@@ -29,34 +29,28 @@ module Baseapp
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
 
-    # Configure the default encoding used in templates for Ruby 1.9.
+    config.i18n.default_locale = :de
+
     config.encoding = "utf-8"
 
-    # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
-    # Use SQL instead of Active Record's schema dumper when creating the database.
-    # This is necessary if your schema can't be completely dumped by the schema dumper,
-    # like if you have constraints or database-specific column types
-    # config.active_record.schema_format = :sql
+    config.assets.precompile += %w( font-awesome-ie7.min.css web.css.sass web.js.coffee
+        user.css.sass. user.css.coffee)
+
+
 
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    config.active_record.whitelist_attributes = false
 
-    # Enable the asset pipeline
     config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
     config.generators do |g|
